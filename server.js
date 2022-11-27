67572e9364d9dc6f674a6f3405e544ba52d83eb8
khaled muhammed <Khaled.Saad@appittuneit.co.uk>
2022-11-27 18:14:24 +0200

    publishing

---
+++ server.js
@@ -0,0 +1,37 @@
+
+require("dotenv").config();
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import authRoutes from "./routes/auth";
+import categoryRoutes from "./routes/category";
+import postRoutes from "./routes/post";
+
+
+
+const morgan = require("morgan");
+
+const app = express();
+
+// db connection
+mongoose
+  .connect(process.env.DATABASE)
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log("DB CONNECTION ERROR: ", err));
+
+// middlewares
+app.use(express.json({ limit: "4mb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(morgan("dev"));
+
+// route middlewares
+app.use("/api", authRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", postRoutes);
+
+
+const port = process.env.PORT || 8000;
+app.listen(port,()=>{
+    console.log(`Server running on port ${port}`)
+})
