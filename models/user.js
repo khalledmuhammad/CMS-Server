2004c64e88e8aef0df292c2eff576a129c9b219d
khaled muhammed <Khaled.Saad@appittuneit.co.uk>
2022-11-27 22:18:11 +0200

    publishing

--- models/user.js
+++ models/user.js
@@ -22,7 +22,7 @@ const userSchema = new Schema(
     },
     role: {
       type: String,
-      default: "Subscriber",
+      default: "Admin",
     },
     image: { type: ObjectId, ref: "Media" },
     website: {
